62a88e25f9c26644f9e2497142942bc7af610098
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-03-23 22:26:05 -0400

    Time: 7 ms (89.32%), Space: 47.2 MB (87.33%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //first sort the array
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        
+        for(int[] interval: intervals){
+            //check if already esistes or not
+            //Also if it exists then check if the first value of 2nd element is greater than the 2nd value of first element
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){
+                merged.add(interval);
+            }
+            else{
+                //update the last value of 1st element with the first value of 2nd element
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
