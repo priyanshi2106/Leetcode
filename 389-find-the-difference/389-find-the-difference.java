5093d17b3309f3a8acef54b9d2b1cfc933fe6577
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-03-28 19:32:13 -0400

    Time: 8 ms (18.55%), Space: 42.2 MB (52.42%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,24 +1,49 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         HashMap<Character,Integer> hm = new HashMap<>();
+        
+        //Brute force
+        
         char ans = ' ';
         char c[] = s.toCharArray();
         
         char tt[] = t.toCharArray();
-        Arrays.sort(c);
-        Arrays.sort(tt);
-        // if(tt.length == c.length){
-        //     return ans;
-        // }
-        int i=0,j=0;
-        while(i<c.length){
-            if(c[i] != tt[i]){
-                return tt[i];
+//         Arrays.sort(c);
+//         Arrays.sort(tt);
+       
+//         int i=0,j=0;
+//         while(i<c.length){
+//             if(c[i] != tt[i]){
+//                 return tt[i];
                 
-            }
-            i++;
+//             }
+//             i++;
            
+//         }
+    //    return tt[i];
+        
+        //Hashmap Time= O(n), space= O(1)
+        for(int i =0; i< c.length;i++){
+            if(!hm.containsKey(c[i])){
+                hm.put(c[i], 1);
+            }
+            else{
+                int val = hm.get(c[i]);
+                hm.put(c[i],val+1);
+            }
+            
         }
-        return tt[i];
+        for(int i= 0; i<tt.length;i++){
+            if(hm.getOrDefault(tt[i],0) == 0){
+                ans = tt[i];
+                break;
+            }
+            else{
+           
+                hm.put(tt[i],hm.get(tt[i])-1);
+            }
+            
+        }
+        return ans;
     }
 }
