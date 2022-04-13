437e968078cdf37d659682413c95db96349a419a
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-04-13 13:08:25 -0400

    Time: 3 ms (84.10%), Space: 42.3 MB (90.41%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,23 +1,38 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[] = new int[nums1.length];
-           boolean found = false;
+        // int ans[] = new int[nums1.length];
+        //    boolean found = false;
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // for(int i = 0; i < nums2.length; i++){
+        //     hm.put(nums2[i],i);
+        // }
+        // int j = 0;
+        // for(int i = 0 ; i < nums1.length; i++){
+        //     for(j = hm.get(nums1[i])+1; j < nums2.length; j++){
+        //         if(nums1[i] < nums2[j]){
+        //             ans[i] = nums2[j];
+        //             break;
+        //         }
+        //     }
+        //     if(j == nums2.length){
+        //         ans[i] = -1;
+        //     }
+        // }
+        // return ans;
+        
+        //Better Approach
+        Stack<Integer> st = new Stack<>();
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i = 0; i < nums2.length; i++){
-            hm.put(nums2[i],i);
-        }
-        int j = 0;
-        for(int i = 0 ; i < nums1.length; i++){
-            for(j = hm.get(nums1[i])+1; j < nums2.length; j++){
-                if(nums1[i] < nums2[j]){
-                    ans[i] = nums2[j];
-                    break;
-                }
+        //First put all the elements into the hashMap and its next greatest element as its value
+        for(int num : nums2){
+            while(!st.isEmpty() && st.peek() < num){
+                hm.put(st.pop(),num);
             }
-            if(j == nums2.length){
-                ans[i] = -1;
-            }
+            st.push(num);
         }
-        return ans;
+        for(int i = 0; i< nums1.length; i++){
+            nums1[i] = hm.getOrDefault(nums1[i],-1);
+        }
+        return nums1;
     }
 }
