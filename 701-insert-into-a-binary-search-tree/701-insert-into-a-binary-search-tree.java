352a190b4aee8de78efb8b96e67fcbdc81f30b6d
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-04-14 17:59:51 -0400

    Time: 0 ms (100.00%), Space: 43.4 MB (80.86%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -15,42 +15,43 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-//         TreeNode tn = new TreeNode(val);
-//         TreeNode t = root;
-//         while(t != null){
-//             if(tn.val < root.val){
-//                 if(t.left == null){
-//                     t.left = tn;
-//                    return root;
-//                 }
-//                 else{
-//                     t = t.left;
-//                 }
-//             }
-//             else  {
-//                 if(t.right == null){
-//                     t.right = tn;
-//                     return root;
-//                 }
-//                 else{
-//                     t = t.right;
-//                 }
-//             }
+        TreeNode tn = new TreeNode(val);
+        TreeNode t = root;
+        
+        while(t!= null){
             
+            if(val < t.val){
+                if(t.left != null){
+                     t = t.left;
+                }
+                else{
+                    t.left = tn;
+                   return root;
+                }
+            }
             
-//         }
+            else{
+                if(t.right != null){
+                     t = t.right;  
+                }
+                else{
+                   t.right = tn;
+                    return root;
+                }
+            }  
+        }
     
-//                 return root;
-        if(root == null){
-            return new TreeNode(val);
-        }
-        if(val < root.val){
-            root.left = insertIntoBST(root.left,val);
-        }
-        else{
-            root.right = insertIntoBST(root.right,val);
-        }
-        return root;
+                return tn;
+        // if(root == null){
+        //     return new TreeNode(val);
+        // }
+        // if(val < root.val){
+        //     root.left = insertIntoBST(root.left,val);
+        // }
+        // else{
+        //     root.right = insertIntoBST(root.right,val);
+        // }
+        // return root;
            
     }
 }
