07d07576726ef3834c393cf325b736edd58525ce
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-04-18 22:01:29 -0400

    Time: 5 ms (98.41%), Space: 44.1 MB (78.05%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,26 +8,36 @@
  * }
  */
 class Solution {
-    TreeNode ans ;
-    public Solution(){
-        this.ans = null;
-    }
+    // TreeNode ans ;
+    // public Solution(){
+    //     this.ans = null;
+    // }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        recurseTree(root, p, q);
-        return ans;
-    }
-    public boolean recurseTree(TreeNode curr, TreeNode p, TreeNode q){
-        if(curr == null){
-            return false;
+        
+        if(root == null || root == p || root == q){
+            return root;
         }
-        //if left recursion returns true then set left as 1
-        int left = recurseTree(curr.left,p,q)?1:0;
-         //if right recursion returns true then set left as 1
-        int right = recurseTree(curr.right,p,q)?1:0;
-        int mid = (curr == p || curr == q)? 1:0;
-        if((mid+left+right) >= 2){
-            ans = curr;
+       TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null){
+            return root;
         }
-        return ((mid+left+right) > 0);
+        return left == null ? right:left;
+        // recurseTree(root, p, q);
+        // return ans;
     }
+    // public boolean recurseTree(TreeNode curr, TreeNode p, TreeNode q){
+    //     if(curr == null){
+    //         return false;
+    //     }
+    //     //if left recursion returns true then set left as 1
+    //     int left = recurseTree(curr.left,p,q)?1:0;
+    //      //if right recursion returns true then set left as 1
+    //     int right = recurseTree(curr.right,p,q)?1:0;
+    //     int mid = (curr == p || curr == q)? 1:0;
+    //     if((mid+left+right) >= 2){
+    //         ans = curr;
+    //     }
+    //     return ((mid+left+right) > 0);
+    // }
 }
