7618352df87541dd473a3302946ab50e403fa34b
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-05-19 21:07:46 -0400

    Time: 2 ms, Memory: 43.5 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,34 +1,35 @@
 class Solution {
     public int findNumbers(int[] nums) {
-//         int count = 0;
-//         int ans = 0;
-//         for(int i = 0; i < nums.length; i++){
-//             int n = nums[i];
-//             while(nums[i] != 0)
-//             {
-//                 //int rem = n%10;
+       
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+             int count = 0;
+            int n = nums[i];
+            while(nums[i] != 0)
+            {
+                //int rem = n%10;
             
-//                 nums[i] = nums[i]/10;
-//                     count++;
-//             }
-//             if(count%2 == 0){
-//                 ans++;
-//             }
-//         }
-//         return ans;
-        
-        int even = 0;
-        
-        for(int i = 0 ; i < nums.length ; i++){
-           int counter = 0;
-            while(nums[i] != 0){
-                 nums[i] /= 10;
-            counter++;
+                nums[i] = nums[i]/10;
+                    count++;
             }
-        if (counter % 2 == 0){
-            even++;
+            if(count%2 == 0){
+                ans++;
+            }
         }
-        }
-    return even;  
+        return ans;
+        
+//         int even = 0;
+        
+//         for(int i = 0 ; i < nums.length ; i++){
+//            int counter = 0;
+//             while(nums[i] != 0){
+//                  nums[i] /= 10;
+//             counter++;
+//             }
+//         if (counter % 2 == 0){
+//             even++;
+//         }
+//         }
+//     return even;  
     }
 }
