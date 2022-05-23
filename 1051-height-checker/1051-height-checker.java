3ce0e57d84adabd89ac3b53e2f00d7cf6e48dffe
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-05-23 19:19:21 -0400

    Time: 0 ms (100.00%), Space: 40 MB (89.21%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -13,20 +13,42 @@ class Solution {
         // }
         // return count;
         
-        int[] bucket = new int[101];
-        for(int number : heights) {
-            bucket[number]++;
+//         int[] bucket = new int[101];
+//         for(int number : heights) {
+//             bucket[number]++;
+//         }
+        
+//         // check the ammount of disparities between the input array and the bucket
+//         int count = 0, index = 0;
+//         for(int i = 1; i <= 100
+//             ; i++) {
+//             while(bucket[i] > 0) {
+//                 if(i != heights[index++]) count++;
+//                 bucket[i]--;
+//             }
+//         }
+//         return count;
+        int[] heightToFreq = new int[101];
+        
+        for (int height : heights) {
+            heightToFreq[height]++;
         }
         
-        // check the ammount of disparities between the input array and the bucket
-        int count = 0, index = 0;
-        for(int i = 1; i <= 100
-            ; i++) {
-            while(bucket[i] > 0) {
-                if(i != heights[index++]) count++;
-                bucket[i]--;
+        int result = 0;
+        int curHeight = 0;
+        
+        for (int i = 0; i < heights.length; i++) {
+            while (heightToFreq[curHeight] == 0) {
+                curHeight++;
             }
+            
+            if (curHeight != heights[i]) {
+                result++;
+            }
+            heightToFreq[curHeight]--;
         }
-        return count;
+        
+        return result;
+
     }
 }
