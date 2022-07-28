1c115452da025905d962bf0e73391919c2e477ef
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-07-28 20:18:30 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (73.80%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        return Math.max(rob(0, nums.length-1, nums), rob(1,nums.length, nums));
+    }
+    public int rob(int start, int end, int nums[]){
+        int prevOne = 0, prevTwo = 0, max = 0;
+       for(int i = start; i < end; i++){
+            max = Math.max(prevOne, prevTwo + nums[i]);
+           prevTwo = prevOne;
+           prevOne = max;
+       }
+        return max;
+    }
+    
+}
