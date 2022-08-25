f8f85da60ea46fef2ec36955d6db20bcd15a0544
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-26 01:57:11 +0530

    Time: 2 ms (90.88%), Space: 43 MB (6.57%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
+        //The basic idea is to put all the right brackets corresponding to left bracket into the stack and if we encounter a right bracket then we check the stack if it is empty? then it is invalid and also we have to check if the topmost element of the stack matches the current elememt in the string. If it does not then it is invalid expression.
         Stack<Character> st = new Stack<>();
-        //check while adding char to stack
         
-	for (char c : s.toCharArray()) {
-		if (c == '(')
-			st.push(')');
-		else if (c == '{')
-			st.push('}');
-		else if (c == '[')
-			st.push(']');
-		else if (st.isEmpty() || st.pop() != c)
-			return false;
-        
-    }
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                st.push(')');
+            }
+           else if(c == '{'){
+               st.push('}');
+           }
+            else if(c == '['){
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.pop() != c){
+                return false;
+            }
+        }
         return st.isEmpty();
     }
 }
