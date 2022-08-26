ef3d3db80f9937c0a82827215fc5dbece72f0156
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-27 05:27:27 +0530

    Time: 7 ms (49.74%), Space: 43.4 MB (76.84%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,16 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //O(nlogn)
+        // //O(nlogn)
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // char c1[] = s.toCharArray();
+        // char c2[] = t.toCharArray();
+        // // sorting O(log n)
+        // Arrays.sort(c1);
+        // Arrays.sort(c2);
+        // for(int i = 0; i < c1.length;i++){
+        //     if(c1[i] != c2[i]){  // O(n)
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        // O(n)
         if(s.length() != t.length()){
             return false;
         }
-        char c1[] = s.toCharArray();
-        char c2[] = t.toCharArray();
-        // sorting O(log n)
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        for(int i = 0; i < c1.length;i++){
-            if(c1[i] != c2[i]){  // O(n)
+        int counter[] = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            //For every char in s we will inc the counter but will dec it for every char in t
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for(int count : counter){
+            if(count != 0){
                 return false;
             }
         }
