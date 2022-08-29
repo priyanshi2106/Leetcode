3ec1dfddbabaf6934522296067adfb1eb016f8d8
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-30 01:25:14 +0530

    Time: 8 ms (74.87%), Space: 67 MB (46.78%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,14 +1,38 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-      HashSet<Integer> hs = new HashSet<>();
-        LinkedList<Integer> ll = new LinkedList<>();
-        int n = nums.length;
-        //first add all the values into hashset
-        for(int i = 0; i < n; i++){
-            hs.add(nums[i]);
-        }
-        for(int i = 1; i < n+1; i++){
-            if(!hs.contains(i)){
+        //Time: O(n)
+        //S: O(n)
+      // HashSet<Integer> hs = new HashSet<>();
+         LinkedList<Integer> ll = new LinkedList<>();
+      //   int n = nums.length;
+      //   //first add all the values into hashset
+      //   for(int i = 0; i < n; i++){
+      //       hs.add(nums[i]);
+      //   }
+      //   for(int i = 1; i < n+1; i++){
+      //       if(!hs.contains(i)){
+      //           ll.add(i);
+      //       }
+      //   }
+      //   return ll;
+        
+        //T:O(n)
+        //S:O(1)
+//         8
+//         [4,3,2,7,8,2,3,1]
+//          0,1, 2, 3, 4, 5, 6, 7   
+            
+//         [-4,-3,-2,-7,-8,2,3,-1]
+//           0. 1. 2. 3  4.5. 6  7
+            
+            for(int i = 0; i < nums.length; i++){
+                int newIndex = Math.abs(nums[i])-1;
+                if(nums[newIndex] > 0){
+                    nums[newIndex]*= -1;
+                }
+            }
+        for(int i = 1; i < nums.length+1; i++){
+            if(nums[i-1] >0){
                 ll.add(i);
             }
         }
