c356947f81b95774a6af8f738d10120dd7353003
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-30 01:02:22 +0530

    Time: 48 ms (9.31%), Space: 69.7 MB (21.28%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,37 +1,17 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-       //Time: O(n)
-        // space: O(n)
-        // HashSet<Integer> hm = new HashSet<>();
-        // LinkedList<Integer> ll = new LinkedList<>();
-        // for(int num : nums){
-        //     hm.add(num);
-        // }
-        // int range = nums.length+1;
-        // for(int i = 1; i< range; i++){
-        //     if(!hm.contains(i)){
-        //         ll.add(i);
-        //     }
-        // }
-        // return ll;
-        
-        //Time: O(n)
-        //space: O(1)
-        
-        List<Integer> ans = new LinkedList<>();
-        for(int i = 0; i < nums.length; i++){
-            int newIndex = Math.abs(nums[i])-1;
-            
-            //check if value at this index is already negative
-            if(nums[newIndex] > 0){
-                nums[newIndex] *= -1;
+      HashSet<Integer> hs = new HashSet<>();
+        LinkedList<Integer> ll = new LinkedList<>();
+        int n = nums.length;
+        //first add all the values into hashset
+        for(int i = 0; i < n; i++){
+            hs.add(nums[i]);
+        }
+        for(int i = 1; i < n+1; i++){
+            if(!hs.contains(i)){
+                ll.add(i);
             }
         }
-        for(int i = 1; i <= nums.length; i++){
-            if(nums[i-1] > 0){
-                ans.add(i);
-            }
-        }
-        return ans;
+        return ll;
     }
 }
