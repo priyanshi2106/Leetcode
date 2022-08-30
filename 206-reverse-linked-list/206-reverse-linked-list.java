a96410d279dc24467aef9b7dc16273c2a4c44cd1
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-31 00:50:51 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (51.11%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -11,16 +11,12 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
-        ListNode nextNode = null; //to store the reference of the next curr node
         ListNode curr = head;
+        ListNode nextNode = null;
         while(curr != null){
-            //first save the reference of the next node
             nextNode = curr.next;
-            //point the curr node tp prev
             curr.next = prev;
-            // change the prev to curr node
             prev = curr;
-            //point the cur Node that stores the reference of next curr to prev node
             curr = nextNode;
         }
         return prev;
