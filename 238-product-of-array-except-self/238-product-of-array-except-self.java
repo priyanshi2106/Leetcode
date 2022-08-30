183d7ac8529a52979319a85e9b5e4c097a8b7db3
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-08-31 02:09:18 +0530

    Time: 3 ms (53.75%), Space: 58.6 MB (6.22%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,21 +1,38 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int left[] = new int[nums.length];
-        int right[] = new int[nums.length];
+        //T:O(n)
+        //S:O(n)
+        // int left[] = new int[nums.length];
+        // int right[] = new int[nums.length];
+        // int ans[] = new int[nums.length];
+        // //taking out product of left side
+        // left[0] = 1;
+        // for(int i = 1; i < nums.length; i++){
+        //     left[i] = nums[i-1] * left[i-1];
+        // }
+        // //taking out product of right side
+        // right[nums.length-1] = 1;
+        // for(int i = nums.length-2; i >= 0; i-- ){
+        //     right[i] = nums[i+1] * right[i+1];
+        // }
+        // //ans array
+        // for(int i = 0; i < nums.length; i++){
+        //     ans[i] = left[i] * right[i];
+        // }
+        // return ans;
+        
+        //T:O(n)
+        //S:O(1)
         int ans[] = new int[nums.length];
-        //taking out product of left side
-        left[0] = 1;
+         ans[0] = 1;
         for(int i = 1; i < nums.length; i++){
-            left[i] = nums[i-1] * left[i-1];
+            ans[i] = nums[i-1] * ans[i-1];
         }
-        //taking out product of right side
-        right[nums.length-1] = 1;
-        for(int i = nums.length-2; i >= 0; i-- ){
-            right[i] = nums[i+1] * right[i+1];
-        }
-        //ans array
-        for(int i = 0; i < nums.length; i++){
-            ans[i] = left[i] * right[i];
+        int right = 1;
+        for(int i = nums.length-1; i >= 0; i--){
+            
+            ans[i] = ans[i] * right;
+            right = right * nums[i];
         }
         return ans;
     }
