57c166fe1105eecbdb877a6e07e5a8b6641b436e
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-09-10 22:31:56 +0530

    Time: 1 ms (14.44%), Space: 42.6 MB (8.00%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,39 +1,41 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans = new LinkedList<>();
-        int left = 0;
-        int right = matrix[0].length-1;
+       List<Integer> ll = new LinkedList<>();
         int up = 0;
         int down = matrix.length-1;
+        int left = 0; 
+        int right = matrix[0].length-1;
         
-        int m = matrix.length, n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         int size = m*n;
-        if(matrix.length == 0 || matrix[0].length == 0 || matrix == null){
-            return ans;
+        
+        if(m == 0 || n == 0 || matrix == null ){
+            return ll;
         }
         
-        while(ans.size() < size){
+        while(ll.size() < size){
             //left to right
-            for(int i = left; i <= right && ans.size() < size; i++){
-                ans.add(matrix[up][i]);
+            for(int i = left; i <= right && ll.size() < size; i++){
+                ll.add(matrix[up][i]);
             }
             up++;
-            //top to bottom
-            for(int i = up; i <= down && ans.size() < size; i++){
-                ans.add(matrix[i][right]);
+            //up to down
+            for(int i = up; i <= down && ll.size() < size; i++){
+                ll.add(matrix[i][right]);
             }
             right--;
-            //right to left
-            for(int i = right; i >= left && ans.size() < size ;i-- ){
-                ans.add(matrix[down][i]);
+            //from right to left
+            for(int i = right; i >= left && ll.size() < size; i--){
+                ll.add(matrix[down][i]);
             }
             down--;
             //down to up
-            for(int i = down; i >= up && ans.size() < size; i--){
-                ans.add(matrix[i][left]);
+            for(int i = down; i >= up && ll.size() < size; i--){
+                ll.add(matrix[i][left]);
             }
             left++;
         }
-        return ans;
+        return ll;
     }
 }
