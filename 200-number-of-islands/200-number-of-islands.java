68051a598ba75e8a716806af3dc6f19dd7099f72
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-09-19 04:27:33 +0530

    Time: 10 ms (22.64%), Space: 57.7 MB (32.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -4,16 +4,16 @@ class Solution {
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    ans ++;
-                    calc(grid, i, j);
+                    ans +=calc(grid, i, j);
+                    
                 }
             }
         }
         return ans;
     }
-    public void calc(char grid[][], int i, int j){
+    public int calc(char grid[][], int i, int j){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0'){
-            return;
+            return 0;
         }
         //To not calculate the same 1 again and again/
         grid[i][j] = '0';
@@ -21,6 +21,6 @@ class Solution {
         calc(grid, i-1, j);
         calc(grid, i, j-1);
         calc(grid, i, j+1);
-       
+       return 1;
     }
 }
