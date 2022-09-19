c9225b4d6b05f973135afd18469be45fbe2b2291
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-09-19 23:30:36 +0530

    Time: 34 ms (13.34%), Space: 54.2 MB (21.94%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -11,60 +11,64 @@ class Solution {
         
         //Method 2
         
-        if(s == null){
-            return null;
-        }
+//         if(s == null){
+//             return null;
+//         }
         
-        char a[] = s.toCharArray();
-        int n = a.length;
+//         char a[] = s.toCharArray();
+//         int n = a.length;
         
-        //Step 1: Reverse the array
-        reverse(a, 0, n-1);
+//         //Step 1: Reverse the array
+//         reverse(a, 0, n-1);
         
-        //Step 2: Reverse each word
-        reverseWordss(a, n);
+//         //Step 2: Reverse each word
+//         reverseWordss(a, n);
         
-        //Step 3: Remove extra spaces
-       return removeSpaces(a, n);
+//         //Step 3: Remove extra spaces
+//        return removeSpaces(a, n);
         
-    }
-    public void reverse(char a[], int i, int j){
-        while(i < j){
-            char t = a[i];
-            a[i++] = a[j];
-            a[j--] = t;
-        }
-    }
-    public void reverseWordss(char a[], int n){
-         int i = 0, j = 0;
-        while (j < n) {
-            while (i < n && a[i] == ' ') i++;
-            j = i;
-            while (j < n && a[j] != ' ') j++;
-            reverse(a, i, j - 1);
-            i = j++;
-        }
-    }
+//     }
+//     public void reverse(char a[], int i, int j){
+//         while(i < j){
+//             char t = a[i];
+//             a[i++] = a[j];
+//             a[j--] = t;
+//         }
+//     }
+//     public void reverseWordss(char a[], int n){
+//          int i = 0, j = 0;
+//         while (j < n) {
+//             while (i < n && a[i] == ' ') {
+//                 i++;  //skipping leading spaces
+//             }
+//             j = i; //start of the word
+//             while (j < n && a[j] != ' '){
+//                j++;   //figuring out end of the word
+//             } 
+//             reverse(a, i, j - 1);
+//             i = j++;
+//         }
+//     }
     
    
-    public String removeSpaces(char a[], int n){
-        int i = 0, j = 0;
-        while(j < n){
-            while(j < n && a[j] == ' '){ // To remove leading spaces
-                j++;
-            }
-            while(j < n && a[j] != ' '){
-                a[i++] = a[j++];
-            }
-            while(j < n && a[j] == ' '){ // To remove trailing spaces
-               j++;
-            }
+//     public String removeSpaces(char a[], int n){
+//         int i = 0, j = 0;
+//         while(j < n){
+//             while(j < n && a[j] == ' '){ // To remove leading spaces
+//                 j++;
+//             }
+//             while(j < n && a[j] != ' '){
+//                 a[i++] = a[j++];
+//             }
+//             while(j < n && a[j] == ' '){ // To remove trailing spaces
+//                j++;
+//             }
             
-            if(j < n){
-                a[i++] = ' '; // keeping 1 extra space for when we use substring 
-            }
-        }
-        return new String(a).substring(0,i);
+//             if(j < n){
+//                 a[i++] = ' '; // keeping 1 extra space for when we use substring 
+//             }
+//         }
+//         return new String(a).substring(0,i);
         
         //Method 3 
         //String builder
@@ -76,5 +80,19 @@ class Solution {
         //     }
         // }
         // return ans.toString().trim();
+        
+        s = s.trim();
+        String a[] = s.split("\\s+");
+        Stack<String> st = new Stack<>();
+        for(String str: a){
+            st.push(str);
+        }
+        String ans ="";
+        while(!st.isEmpty()){
+            ans += st.pop();
+            ans +=" ";
+        }
+        ans = ans.trim();
+        return ans;
     }
 }
