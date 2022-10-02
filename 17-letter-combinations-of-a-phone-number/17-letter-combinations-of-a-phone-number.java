2cf0f6d3dd232d2468ec14073baf193c9f6557d7
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-10-03 00:40:24 +0530

    Time: 1 ms (96.42%), Space: 42.5 MB (67.30%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,4 +1,6 @@
 class Solution {
+    //T:O(N*4^N)
+    //S:O(N) -> length of the digits
      Map<Character,String> hm = Map.of('2', "abc", '3', "def", '4', "ghi", '5',         "jkl", '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
     
     public List<String> letterCombinations(String digits) {
