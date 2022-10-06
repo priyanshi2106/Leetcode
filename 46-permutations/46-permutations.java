87b3b2e803b6234095db4f138a376d5f087a9d9c
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-10-07 00:55:08 +0530

    Time: 2 ms (78.91%), Space: 44.5 MB (67.85%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        //T: O((n!)n
+        //T: O((n!)n)
         //S: O(n!)
         List<List<Integer>> res = new ArrayList<>();
         if(nums.length == 0 || nums == null){
@@ -13,6 +13,7 @@ class Solution {
     }
     public void helper(List<List<Integer>> res, List<Integer> permutation, boolean[] used, int[] nums){
         if(permutation.size() == nums.length){
+            //storing deep copy because arrayList is a reference data type toh whenever we make changes then all the changes will be reflected in the orig array and we do not want that.
             res.add(new ArrayList<>(permutation));return;
         }
         for(int i = 0; i < nums.length; i++){
