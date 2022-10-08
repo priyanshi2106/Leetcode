bae4e4a86bc2067f7b76e655d80d7c2f5b44af5a
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-10-08 09:09:50 +0530

    Time: 1 ms (87.37%), Space: 44.3 MB (14.60%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i = 0; 
+        int j = nums.length-1;
+        while(i <= j){
+            int mid = (i+j)/2;
+            if(target == nums[mid]){
+                return true;
+            }
+            // if we know that left side is sorted and right is not and mid is in left side
+            if(nums[mid] > nums[i] || nums[mid] > nums[j]){
+                if(target >= nums[i] && target < nums[mid]){
+                    j = mid-1;
+                }
+                else{
+                    i = mid+1;
+                }
+            }
+            //right is sorted
+            else if(nums[mid] < nums[j] || nums[mid] < nums[i]){
+                if(target > nums[mid] && target <= nums[j]){
+                    i = mid+1;
+                }
+                else{
+                    j = mid-1;
+                }
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+    }
+}
