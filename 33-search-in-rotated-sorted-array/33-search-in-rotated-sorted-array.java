53ed3df6851535ac245e39ddde9e5836b9a0ce7e
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-10-08 09:17:27 +0530

    Time: 1 ms (79.89%), Space: 43.1 MB (11.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
+        //T: O(logN)
+        //S:O(1)
         //Binary Search
-        int i = 0;
+       int i = 0; 
         int j = nums.length-1;
-        //int ans = 0;
         while(i <= j){
             int mid = (i+j)/2;
-            if(nums[mid] == target){
-                
+            if(target == nums[mid]){
                 return mid;
             }
-            //first half is non-rotated
-            else if(nums[mid] >= nums[i]){
+            // if we know that left side is sorted and right is not and mid is in left side
+            if(nums[mid] > nums[i] || nums[mid] > nums[j]){
                 if(target >= nums[i] && target < nums[mid]){
                     j = mid-1;
                 }
@@ -19,16 +19,20 @@ class Solution {
                     i = mid+1;
                 }
             }
-           else{
-               //first half is rotated
-               if(target <= nums[j] && target > nums[mid]){
-                   i = mid+1;
-               }
-               else{
-                   j = mid-1;
-               }
-           }
-            
+            //right is sorted
+            else if(nums[mid] < nums[j] || nums[mid] < nums[i]){
+                if(target > nums[mid] && target <= nums[j]){
+                    i = mid+1;
+                }
+                else{
+                    j = mid-1;
+                }
+            }
+            else{
+                //If we got here, that means nums[i] == nums[mid] == nums[j], then shifting out
+                // remove duplicates
+                j--;
+            }
         }
         return -1;
     }
