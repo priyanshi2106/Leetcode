170a17687b7b1e4518570f2de0e6de7650952594
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-10-15 00:49:47 +0530

    Time: 2 ms (84.55%), Space: 49.9 MB (66.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,16 +1,36 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-      for(int i = 0; i < numbers.length; i++){
-             hm.put(numbers[i], i+1);
-        }
-        for(int i = 0; i < numbers.length; i++){
-            int val = target - numbers[i];
-            if(hm.containsKey(val)){
-                return new int[]{ i+1, hm.get(val)};
+        //Not really efficient solution because we are using O(n) space to store the           numbers in the hashmap
+        //T: O(n)
+        //S: O(n)
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//       for(int i = 0; i < numbers.length; i++){
+//              hm.put(numbers[i], i+1);
+//         }
+//         for(int i = 0; i < numbers.length; i++){
+//             int val = target - numbers[i];
+//             if(hm.containsKey(val)){
+//                 return new int[]{ i+1, hm.get(val)};
                 
-            }
+//             }
             
+//         }
+//         return new int[]{-1,-1};
+        
+        //Using two pointers
+        int i = 0;
+        int j = numbers.length-1;
+        while(i <= j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target){
+                return new int[]{i+1, j+1};
+            }
+            else if(sum < target){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
         return new int[]{-1,-1};
     }
