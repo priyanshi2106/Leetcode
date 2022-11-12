fd2d3a45ce5880bd588523d78593c0e512af4f72
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-11-13 04:06:21 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (85.47%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+        //we find the min(height at left pt and height at right pt)
+        int left = 0;
+        int right = height.length-1;
+        int left_max = 0;
+        int right_max = 0;
+        int ans = 0;
+        
+        while(left < right){
+            if(height[left] < height[right]){//this condition checks the min of height at left and right
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }
+                else{
+                    ans+=(left_max-height[left]);
+                }
+                
+                left++;
+            }
+            else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }
+                else{
+                    ans+=(right_max-height[right]);
+                }
+                
+                right--;
+                
+            }
+        }
+        return ans;
+    }
+}
