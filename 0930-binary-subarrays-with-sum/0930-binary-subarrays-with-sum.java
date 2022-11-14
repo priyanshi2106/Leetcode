378b18f690d04ed93ca922cbdfbf775aea57aaab
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-11-14 05:51:46 +0530

    Time: 3 ms (92.31%), Space: 55.8 MB (58.29%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,21 +1,24 @@
 class Solution {
+    //Sliding window technique
+    //T: O(n)
+    //S: O(1)
     public int numSubarraysWithSum(int[] nums, int goal) {
         return atMost(nums, goal) - atMost(nums, goal-1);
     }
     public int atMost(int nums[], int goal){
-        int l = 0; 
-        int r = 0;
+        int left = 0; 
+        int right = 0;
         int sum = 0; //keeps the track of sum
         int res = 0;
-        while(r < nums.length){
-            sum = sum + nums[r];
-            while(l <= r && sum > goal){
+        while(right < nums.length){
+            sum = sum + nums[right];
+            while(left <= right && sum > goal){
                 //we need to dec the sum because it exceeds the goal
-                sum = sum - nums[l];
-                l++;
+                sum = sum - nums[left];
+                left++;
             }
-            res = res+(r-l);
-            r++;
+            res = res+(right-left);
+            right++;
         }
         return res;
     }
