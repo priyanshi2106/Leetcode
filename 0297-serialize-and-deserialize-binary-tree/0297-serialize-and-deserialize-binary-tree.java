88f666554c4a519018f16b6fc574a0157bde7511
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2022-11-16 08:02:05 +0530

    Time: 22 ms (74.58%), Space: 44.9 MB (88.29%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -9,52 +9,34 @@
  */
 public class Codec {
 
-//     // Encodes a tree to a single string.
-//     public String serialize(TreeNode root) {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
    
-//         if(root == null){
-//             return null;
-//         }
+        if(root == null){
+            return "X";
+        }
         
-//         return root.val + ","+ serialize(root.left) +"," + serialize(root.right);
-//     }
-
-//     // Decodes your encoded data to tree.
-//     public TreeNode deserialize(String data) {
-//         Queue<String> q = new LinkedList<String>();
-//         //Populate this queue with the string values
-//         q.addAll(Arrays.asList(data.split(",")));
-//         //s.split will convert string to array so we need to convert that array to           list to be able to put it into queue
-        
-//         return helper(q);
-//     }
-//     public TreeNode helper(Queue<String> q){
-//         String value = q.poll();
-//         if(value.equals("null")){
-//             return null;
-//         }
-//         TreeNode newNode = new TreeNode(Integer.valueOf(value));
-//         newNode.left = helper(q);
-//         newNode.right = helper(q);
-//         return newNode;
-//     }
-     public String serialize(TreeNode root) {
-        if (root == null) return "#";
-        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+        return root.val + ","+ serialize(root.left) +"," + serialize(root.right);
     }
 
+    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
-        return helper(queue);
+        Queue<String> q = new LinkedList<String>();
+        //Populate this queue with the string values
+        q.addAll(Arrays.asList(data.split(",")));
+        //s.split will convert string to array so we need to convert that array to           list to be able to put it into queue
+        
+        return helper(q);
     }
-    
-    private TreeNode helper(Queue<String> queue) {
-        String s = queue.poll();
-        if (s.equals("#")) return null;
-        TreeNode root = new TreeNode(Integer.valueOf(s));
-        root.left = helper(queue);
-        root.right = helper(queue);
-        return root;
+    public TreeNode helper(Queue<String> q){
+        String v = q.poll();
+        if(v.equals("X")){
+           return null;
+        }
+        TreeNode newNode = new TreeNode(Integer.valueOf(v));
+        newNode.left = helper(q);
+        newNode.right = helper(q);
+        return newNode;
     }
 }
 
