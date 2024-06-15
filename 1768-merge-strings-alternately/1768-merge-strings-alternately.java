75ec441e7ad1c969fe85883b538f63ad6315322f
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2024-06-15 18:42:12 -0400

    Time: 1 ms (85.71%), Space: 41.6 MB (52.53%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,33 +1,23 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        // O(T) : O(m+n)
-        // O(S) : O(1)
-        int i = 0, j = 0;
-        
-        //use stringBuilder for inplace string modifiction for faster execution
-        
-        // String ans = "";
+        char w1[] = word1.toCharArray();
+        char w2[] = word2.toCharArray();
+        int k = 0;
+        int l = 0;
         StringBuilder ans = new StringBuilder();
-        // char w1[] = word1.toCharArray();
-        // char w2[] = word2.toCharArray();
-        int len = word1.length() + word2.length();
-        for (int k = 0; k < len; k++){
-            if(i < word1.length() && j < word2.length()){
-                ans.append(word1.charAt(i)).append(word2.charAt(i));
-                // ans = ans + w1[i] + w2[j];
-                i++;
-                j++;
+        for(int i = 0; i < word1.length()+word2.length() ; i++){
+            if(k < word1.length() && l< word2.length()){
+                ans.append(w1[k]).append(w2[l]);
+                k++;
+                l++;
             }
-            else if(i >= word1.length() && j < word2.length()) {
-                ans.append(word2.charAt(j));
-                //ans = ans + w2[j];
-                j++;
-                
+            else if(k >= word1.length() && l < word2.length()){
+                ans.append(w2[l]);
+                l++;
             }
-            else if(i < word1.length() && j >= word2.length()){
-                ans.append(word1.charAt(i));
-                //ans = ans + w1[i];
-                i++;
+            else if(k < word1.length() && l >= word2.length()){
+                ans.append(w1[k]);
+                k++;
             }
         }
         return ans.toString();
