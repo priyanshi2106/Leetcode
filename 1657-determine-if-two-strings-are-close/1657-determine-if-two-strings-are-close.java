2f98438bb519b4f2ed21e883137022c528ee0b1d
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2024-11-22 00:59:38 -0500

    Time: 9 ms (98.73%), Space: 45.1 MB (98.16%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -5,29 +5,45 @@ class Solution {
         if(len1 != len2){
             return false;
         }
-        HashMap<Character, Integer> hm = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
-        for(char w1: word1.toCharArray()){
-            if(!hm.containsKey(w1)){
-                hm.put(w1,1);
+        int w1[] = new int[26];
+        int w2[] = new int[26];
+        for(char c: word1.toCharArray()){
+            w1[c-'a']++;
+        }
+        for(char c: word2.toCharArray()){
+            w2[c-'a']++;
+        }
+        for(int i = 0; i < 26; i++){
+            if((w1[i] == 0 && w2[i] > 0) || w2[i] == 0 && w1[i]>0){
+                return false;
             }
-            hm.put(w1, hm.get(w1)+1);
         }
-        for(char w2: word2.toCharArray()){
-            if(!hm2.containsKey(w2)){
-               hm2.put(w2, 1);
-            }
-            hm2.put(w2, hm2.get(w2)+1);
-        }
-        //check keys for both hashmaps
-        if(!hm.keySet().equals(hm2.keySet())){
-            return false;
-        }
-        //sort the values for both hashmaps
-        List<Integer> l1 = new ArrayList<>(hm.values());
-        List<Integer> l2 = new ArrayList<>(hm2.values());
-        Collections.sort(l1);
-        Collections.sort(l2);
-        return l1.equals(l2);
+        Arrays.sort(w1);
+        Arrays.sort(w2);
+        return Arrays.equals(w1,w2);
+        // HashMap<Character, Integer> hm = new HashMap<>();
+        // HashMap<Character, Integer> hm2 = new HashMap<>();
+        // for(char w1: word1.toCharArray()){
+        //     if(!hm.containsKey(w1)){
+        //         hm.put(w1,1);
+        //     }
+        //     hm.put(w1, hm.get(w1)+1);
+        // }
+        // for(char w2: word2.toCharArray()){
+        //     if(!hm2.containsKey(w2)){
+        //        hm2.put(w2, 1);
+        //     }
+        //     hm2.put(w2, hm2.get(w2)+1);
+        // }
+        // //check keys for both hashmaps
+        // if(!hm.keySet().equals(hm2.keySet())){
+        //     return false;
+        // }
+        // //sort the values for both hashmaps
+        // List<Integer> l1 = new ArrayList<>(hm.values());
+        // List<Integer> l2 = new ArrayList<>(hm2.values());
+        // Collections.sort(l1);
+        // Collections.sort(l2);
+        // return l1.equals(l2);
     }
 }
