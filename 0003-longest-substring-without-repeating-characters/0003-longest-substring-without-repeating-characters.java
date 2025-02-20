104c63b8d4c5ddd50a85c47be647fecaeeb7e362
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2025-02-20 21:37:34 +0530

    Time: 6 ms (70.55%), Space: 44.6 MB (75.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -9,7 +9,7 @@ class Solution {
             //check for the char in hm, if present that means we already have that in pur substring
             if(!hm.containsKey(s.charAt(j))){
                   hm.put(s.charAt(j), j);
-                max = Math.max(max, hm.size());
+                max = Math.max(max, j-i+1);
                 j++;
             }
             else{
@@ -19,5 +19,26 @@ class Solution {
             }
         }
         return max;
+        ////T: O(n)
+        //S: O(n) -> bcs worst case the substring is the whole original string itself
+        // int a_pointer = 0;
+        // int b_pointer = 0;
+        // int max = 0;
+        // HashSet<Character> hs = new HashSet<>();
+        // //using hashset because lookup time for hashset is O(1)
+        // while(b_pointer < s.length()){
+        //     if(!hs.contains(s.charAt(b_pointer))){
+        //         hs.add(s.charAt(b_pointer));
+        //         b_pointer++;
+        //         max = Math.max(max, hs.size());
+                
+        //     }
+        //     else{
+        //         hs.remove(s.charAt(a_pointer));
+        //         //max--;
+        //         a_pointer++;
+        //     }
+        // }
+        // return max;
     }
 }
