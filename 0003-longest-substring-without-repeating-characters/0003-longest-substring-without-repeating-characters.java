0293080722624822fd33033e3aec24050319bbef
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2025-10-18 16:25:35 -0700

    Time: 6 ms (70.39%), Space: 45 MB (22.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int max = 0;
-        int j = 0;
-        int i = 0;
-        int count = 0;
-        while(i <= j && j < s.length()){
-            //check for the char in hm, if present that means we already have that in pur substring
-            if(!hm.containsKey(s.charAt(j))){
-                  hm.put(s.charAt(j), j);
-                max = Math.max(max, j-i+1);
-                j++;
-            }
-            else{
-                //count++;
-                hm.remove(s.charAt(i));
-                i++;
-            }
+       int i = 0;
+       int j = 0;
+       int max = 0;
+       HashMap<Character, Integer> hm = new HashMap<>();
+    
+       while(j < s.length()){
+        if(!hm.containsKey(s.charAt(j))){
+            hm.put(s.charAt(j), j);
+            j++;
+            max = Math.max(max, hm.size());
         }
-        return max;
+        else{
+            hm.remove(s.charAt(i));
+            i++;
+        }
+       }
+         return max;
         ////T: O(n)
         //S: O(n) -> bcs worst case the substring is the whole original string itself
         // int a_pointer = 0;
