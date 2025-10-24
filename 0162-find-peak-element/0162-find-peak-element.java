83b785eb3b6ab917eef596938970adda93a2db13
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2025-10-24 00:42:22 -0400

    Time: 0 ms (100%), Space: 42.4 MB (73.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,34 +1,23 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int i = 0;
-        int j = nums.length-1;
-        int max = -1;
-        if(nums.length == 1){
-            return 0;
+        if(nums.length == 1) return 0; // single element
+        
+        int n = nums.length;
+        
+		// check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+		
+		// search in the remaining array
+        int start = 1;
+        int end = n-2;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
-        if(nums[0] > nums[1]){
-            return 0;
-        }
-        if(nums[j] > nums[j-1]){
-            return j;
-        }
-        i = 1;
-        j = nums.length-2;
-        while(i <= j){
-            int mid = (i+j)/2;
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]){ // mid is the peak
-                max = mid;
-                return max;
-            }
-            else if(nums[mid] < nums[mid+1]){
-                i = mid+1;
-            }
-            else if(nums[mid] < nums[mid-1]){
-                j = mid-1;
-            }
-            
-            
-        }
-        return max;
+        return -1; // dummy return statement
     }
 }
