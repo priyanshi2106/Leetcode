210e95eb81b3cc1f8bd725c87fe962c55be55cd7
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2025-10-29 22:17:30 -0400

    Time: 0 ms (100%), Space: 41 MB (78.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        //This dp array will contain the max value up untill the last index that we            can get as an ans
-        int dp[] = new int[nums.length+1];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
-        }
-        //Not returning dp[nums.length-1] bcs we started putting our ans from 1st             index
-        return dp[nums.length];
+        int n = nums.length;
+      if(nums.length == 0){
+        return 0;
+      }
+      int dp[] = new int [n+1];
+      dp[0] = 0;
+      dp[1] = nums[0];
+      for(int i = 1; i < n; i++){
+        dp[i+1] = Math.max(dp[i], nums[i] + dp[i-1]);
+      }
+      return dp[nums.length];
     }
 }
