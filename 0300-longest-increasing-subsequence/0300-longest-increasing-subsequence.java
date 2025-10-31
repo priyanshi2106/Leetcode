1ac7029718fa09070af99a6e8924ee6cb4ccde57
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2025-10-31 16:53:20 -0400

    Time: 40 ms (65.41%), Space: 46.2 MB (24.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,58 +1,21 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-//         //T: O(n^2)
-//         // S:O(N)
-//         int dp[] = new int[nums.length];
-//         //fill this dp array with values of 1 which is initial value
-//         Arrays.fill(dp,1);
-//         for(int i = 1; i < nums.length; i++){
-//             for(int j = 0; j < i; j++){
-                
-//                 if(nums[i] > nums[j]){
-//                     dp[i] = Math.max(dp[i], dp[j]+1);
-//                 }
-//             }
-//         }
-//         int longest = 0;
-//         for(int c:dp){
-//             longest = Math.max(longest, c);
-//         }
-//         return longest;
-        
-        //Binary Search
-        //T: O(nlogn)
-        //S: O(n)
-        ArrayList<Integer> sub = new ArrayList<>();
-        sub.add(nums[0]);
-        
-        for(int i = 1; i < nums.length; i++){
-            int num = nums[i];
-            if(num > sub.get(sub.size()-1)){
-                sub.add(num);
-            }
-            else{
-                int j = binarySearch(sub, num);
-                sub.set(j, num);
+       int dp[] = new int[nums.length+1];
+       Arrays.fill(dp, 1);
+       for(int i = 1; i < nums.length; i++){
+        for(int j = 0; j < i; j++){
+            if(nums[i] > nums[j]){
+                dp[i] = Math.max(dp[i], dp[j]+1);
             }
         }
-        return sub.size();
-    }
-    public int binarySearch(ArrayList<Integer> sub, int num){
-        int left = 0;
-        int right = sub.size()-1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(sub.get(mid) == num){
-                return mid;
-            }
-            if(sub.get(mid) < num){
-                left = mid+1;
-            }
-            else{
-                right = mid;
-            }
+       }
+       //find the max from dp and return that
+       int longest = 0; 
+       for(int i: dp){
+        if(i > longest){
+            longest = i;
         }
-        return right;
+       }
+       return longest;
     }
-    
 }
