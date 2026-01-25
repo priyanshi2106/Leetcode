bc563f225f002822533bf40eb87f31b9e4cbf603
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-01-25 17:57:22 -0500

    Time: 5 ms (50.55%), Space: 45.2 MB (99.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,19 +3,18 @@ class Solution {
         // x = nums[i]-target
         // check for x in hm, if we find it we store the ans if not we move forward and
         // add nums[i] to hm with index as values
-
-        HashMap<Integer, Integer> hm = new HashMap<>();
         int ans[] = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            int x = target - nums[i];
-            if (hm.containsKey(x)) {
-                ans[0] = hm.get(x);
-                ans[1] = i;
-                return ans;
-            } else {
-                hm.put(nums[i], i);
-            }
+       HashMap<Integer, Integer> hm = new HashMap<>();
+       for(int i = 0; i < nums.length; i++){
+        int x = target - nums[i];
+        if(!hm.containsKey(x)){
+            hm.put(nums[i], i);
         }
-         return ans;
+        else{
+            ans[0] = hm.get(x);
+            ans[1] = i;
+        }
+       }
+       return ans;
     }
 }
