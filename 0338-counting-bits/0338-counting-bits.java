c0efa5b622513e31a93f5bafb9023124a9bbf862
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-02-07 18:11:14 -0500

    Time: 2 ms (96.53%), Space: 48.7 MB (67.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,15 +1,30 @@
 class Solution {
     public int[] countBits(int n) {
-        int ans[] = new int[n+1];
-        for(int i = 0; i <= n; i++){
-            int count = 0;
-            int temp = i;
-            while(temp != 0){
-                count += temp & 1;
-                temp >>>=1;
+        // int ans[] = new int[n+1];
+        // for(int i = 0; i <= n; i++){
+        //     int count = 0;
+        //     int temp = i;
+        //     while(temp != 0){
+        //         count += temp & 1;
+        //         temp >>>=1;
+        //     }
+        //     ans[i] = count;
+        // }
+        // return ans;
+
+        // dp solution
+        //we need to offset the bit by most significant bit
+        // 1+ dp[n-most significant bit]
+        //how do we find/compute this msb? msb*2 == i? if yes update msb
+
+        int dp[] = new int[n+1];
+        int msb = 1;
+        for(int i = 1; i <=n; i ++){
+            if(msb *2 == i){
+                msb = i;
             }
-            ans[i] = count;
+            dp[i] = 1+dp[i-msb];
         }
-        return ans;
+        return dp;
     }
 }
