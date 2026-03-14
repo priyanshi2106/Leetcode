1b843e1188fb0aaffbee0554d1d6c78fc2e6091c
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-14 16:06:08 -0400

    Time: 8 ms (94.65%), Space: 48.5 MB (50.85%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,18 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // Instead of manually iterating on every room that's been allocated and
-        // checking if the room is available or not, we can keep all the rooms in a min
-        // heap where the key for the min heap would be the ending time of meeting.
-       Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-       PriorityQueue<Integer> pq = new PriorityQueue<>();
-       pq.add(intervals[0][1]);
-       for(int i = 1; i < intervals.length; i++){
-        //no conflict
-        if(intervals[i][0] >= pq.peek()){
-            pq.poll();
+        Arrays.sort(intervals, (a,b) -> a[0] -b[0]);
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+        minPq.add(intervals[0][1]);
+
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] >= minPq.peek()){
+                //can use the same room
+                minPq.poll();
+            }
+            //add the last meeting in here
+            minPq.add(intervals[i][1]);
         }
-        pq.add(intervals[i][1]);
-       }
-       return pq.size();
+        return minPq.size();
     }
 }
