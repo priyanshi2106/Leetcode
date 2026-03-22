13cd9eba4c3bc1ddcf483071f5608c07c8b42146
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-22 16:39:43 -0400

    Time: 156 ms (15.28%), Space: 43.1 MB (60.67%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,10 +1,30 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
+        //t:O(n^2)
+        // int ans = 0;
+        // while(s.indexOf("01") >= 0){
+        //     s = s.replace("01", "10");
+        //     ans++;
+        // }
+        // return ans;
+
         int ans = 0;
-        while(s.indexOf("01") >= 0){
-            s = s.replace("01", "10");
-            ans++;
+        StringBuilder sb = new StringBuilder(s);
+        while(swapped(sb)){
+            ans ++;
         }
         return ans;
+    }
+    public boolean swapped(StringBuilder s){
+        boolean swappedFlag = false;
+        for(int i = 0; i < s.length() - 1; i++){
+            if(s.charAt(i) == '0' && s.charAt(i+1) == '1'){
+                s.setCharAt(i, '1');
+                s.setCharAt(i+1, '0');
+                i++;
+                swappedFlag = true;
+            }
+        }
+        return swappedFlag;
     }
 }
