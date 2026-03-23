650be464be61859112ec0b3f446beecec729b8d5
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-22 22:42:10 -0400

    Time: 9 ms (20.78%), Space: 46.8 MB (91.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, List<Integer>> adjList = new HashMap<>();
-        int indegree[] = new int[numCourses];
-
-        for(int edge[] : prerequisites){
+       HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+        int indegre[] = new int[numCourses];
+        for(int[] edge: prerequisites){
             int src = edge[1];
             int dest = edge[0];
             adjList.putIfAbsent(src, new ArrayList<>());
             adjList.get(src).add(dest);
-            indegree[dest]++;
+            indegre[dest]++;
         }
-
+        //add all 0 indegrees to queue
         Queue<Integer> q = new LinkedList<>();
-        for(int i =0; i < numCourses; i++){
-            if(indegree[i] == 0){
+        for(int i = 0; i < numCourses; i++){
+            if(indegre[i] == 0){
                 q.add(i);
             }
         }
         int count = 0;
-
+        //count is inc by 1 everytimewe pop a node out of a queue and its indegree is 0
         while(!q.isEmpty()){
             int node = q.poll();
             count++;
-            //check neighbors
             if(!adjList.containsKey(node)){
                 continue;
             }
-            for(int neighbor : adjList.get(node)){
-                indegree[neighbor]--;
-                if(indegree[neighbor] == 0){
-                    q.add(neighbor);
+                //check neighbors
+                for(int neighbor: adjList.get(node)){
+                    indegre[neighbor]--;
+                    if(indegre[neighbor] == 0){
+                        q.add(neighbor);
+                    }
                 }
-            }
+            
         }
         return count == numCourses;
+
+
     }
 }
