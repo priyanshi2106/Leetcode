5d2861769aa302df61b0eb86e96e5f409379d0df
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-26 20:39:38 -0400

    Time: 13 ms (79.59%), Space: 51.9 MB (5.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -17,41 +17,44 @@ class Solution {
         //     ans[i] = list.get(i);
         // }
         // return ans;
-        
+
         //                        BUCKET SORT
         //T: O(n)
         //S: O(n)
-      HashMap<Integer, Integer> hm = new HashMap<>();
+
         int ans[] = new int[k];
-      for(int n: nums){
-        if(!hm.containsKey(n)){
-            hm.put(n,1);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int n : nums) {
+            if (!hm.containsKey(n)) {
+                hm.put(n, 1);
+            } else {
+                hm.put(n, hm.get(n) + 1);
+            }
         }
-        else{
-            hm.put(n, hm.get(n)+1);
+
+        //Make an array of lists named buckets so that you can store the no in the particular freq represented by the index. Doing so( making the index as freq) will make it easier for us to find the top k elements bcs then we can iterate from the end and see if that index has any element stored in it.
+
+        //Creating a bucket and they will store the list of numbers of that freq
+        List<Integer>[] buckets = new List[nums.length + 1];
+        //The size is length + 1 bcs no element is going to have freq of 0 so that is going to remain empty.
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
         }
-      }
-
-      List<Integer> [] bucket = new List[nums.length+1];
-      for(int i = 0; i < bucket.length; i++){
-        bucket[i] = new ArrayList<>();
-      }
-      //add to bucket
-      for(int key: hm.keySet()){
-        bucket[hm.get(key)].add(key);
-      }
-
-      List<Integer> flattened = new ArrayList<>();
-
-      for(int i = bucket.length-1; i>=0; i--){
-        for(int n: bucket[i]){
-            flattened.add(n);
+        for (int key : hm.keySet()) {
+            buckets[hm.get(key)].add(key);
         }
-      }
-      for(int i = 0 ; i < k; i++){
-        ans[i] = flattened.get(i);
-      }
-      return ans;
+        //now create the flattened list of nums in sorted manner and then we will store the values of this list to ans array.
+
+        List<Integer> flattened = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            for (int num : buckets[i]) { // This loop is here bcs there might be more than 1 element in bucket[i]
+                flattened.add(num);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            ans[i] = flattened.get(i);
+        }
+        return ans;
 
     }
-} 
+}
