6e251cf248e3e2792f51b5893c4035850a67d381
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-29 19:04:28 -0400

    Time: 0 ms (100%), Space: 47.4 MB (59.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,13 +4,21 @@ class Solution {
         // 0^0 -> 0
         //1^0 -> 1
         //1^1 - > 0
-        int missing = nums.length;
-        //Initialize missing = nums.length to include the last number n, then XOR it with all indices and array elements; all duplicates cancel and only the missing number remains.
+        // int missing = nums.length;
+        // //Initialize missing = nums.length to include the last number n, then XOR it with all indices and array elements; all duplicates cancel and only the missing number remains.
 
-        //index only goes till length-1 
-        for (int i = 0; i < nums.length; i++) {
-            missing ^= i ^ nums[i];
+        // //index only goes till length-1 
+        // for (int i = 0; i < nums.length; i++) {
+        //     missing ^= i ^ nums[i];
+        // }
+        // return missing;
+
+int n = nums.length;
+        int expSum = n*(n+1)/2;
+        int actualSum = 0;
+        for(int i = 0; i < n; i++){
+            actualSum += nums[i];
         }
-        return missing;
+        return (expSum-actualSum);
     }
 }
