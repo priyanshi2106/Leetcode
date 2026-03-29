1a8a8f89cae405b66efed04dfeaefb9226c4efbe
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-28 23:47:09 -0400

    Time: 2 ms (90.33%), Space: 44.4 MB (20.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -4,15 +4,14 @@ class Solution {
         // compared to dfs
         // S:O(n*m)
         // t: O(n*m)
-        int[][] direction = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
         Queue<int[]> q = new LinkedList<>();
         int countFresh = 0;
-        int ans = 0;
+        int directions[][] = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 2) {
-                    //found rotten add it to queue
                     q.add(new int[] { i, j });
+                    // bfs(grid, i, j, directions)
                 } else if (grid[i][j] == 1) {
                     countFresh++;
                 }
@@ -21,34 +20,33 @@ class Solution {
         if (countFresh == 0) {
             return 0;
         }
-
+        int ans = 0;
+        // int rotten = false;
         while (!q.isEmpty()) {
-            boolean foundRottenThisRound = false;
-            //go through queue and see if it can rott another orange
             int size = q.size();
-            for (int k = 0; k < size; k++) {
+            boolean rottenThisRound = false; // Flag to track if any fresh orange at each level
+            for (int i = 0; i < size; i++) {
                 int curr[] = q.poll();
                 int row = curr[0];
                 int col = curr[1];
-                //check its neighbors
-                for (int[] dir : direction) {
+                // check all 4 neighbors of this element
+                for (int dir[] : directions) {
                     int newRow = row + dir[0];
                     int newCol = col + dir[1];
-                    if (newRow >= 0 && newCol >= 0 && newCol < grid[0].length && newRow < grid.length
+                    if (newCol >= 0 && newRow >= 0 && newCol < grid[0].length && newRow < grid.length
                             && grid[newRow][newCol] == 1) {
-                        foundRottenThisRound = true;
-                        grid[newRow][newCol] = 2;
                         q.add(new int[] { newRow, newCol });
+                        grid[newRow][newCol] = 2;
+                        rottenThisRound = true;
                         countFresh--;
                     }
                 }
             }
-            if (foundRottenThisRound) {
-                ans++;
-
+            if (rottenThisRound) {
+                ans++; // Increment minutes only if we rotted at least one orange
             }
         }
-
         return countFresh == 0 ? ans : -1;
+
     }
 }
