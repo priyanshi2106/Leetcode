09f1936cd0d7e01330fe2d9c2367306448eeb5df
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-03-29 23:11:03 -0400

    Time: 0 ms (100%), Space: 43.4 MB (68.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,20 +13,21 @@ class Solution {
         //         If ListNode has only one element
         // If length of ListNode is equal to n, you will have to remove the first Node
         // Every other condition will be hadled by your implementation of remove node
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        int length = 0;
-        ListNode first = head;
-        while(first != null){
-            length++;
-            first = first.next;
-        }
-        first = dummy;
-        for(int i = 0; i< length-n; i++){
-            first = first.next;
-        }
-        first.next = first.next.next;
-        return dummy.next;
+       ListNode dummy = new ListNode(-1);
+       dummy.next = head;
+       ListNode first = head;
+       int length = 0;
+       //count length
+       while(first != null){
+        length++;
+        first = first.next;
+       }
+       first = dummy;
+       for(int i = 0; i < length-n;i++){
+        first = first.next;
+       }
+       first.next = first.next.next;
+       return dummy.next;
 
     }
 }
