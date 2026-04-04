a80b4de4414a536c3e6a7e819d0b06da57564973
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-04 16:38:28 -0400

    Time: 1 ms (99.9%), Space: 45.8 MB (14.72%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,25 +1,23 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0; 
-        int lastIndex = 0;//to re-create the char array;
-        while( i < chars.length){
-            int j = i+1;
-            while(j < chars.length && chars[i] == chars[j]){
+        int i = 0;
+        int j = 0;
+        int lastIndex = 0;
+        while(j < chars.length){
+            j=i+1;
+            while(j < chars.length && chars[j] == chars[i]){
                 j++;
             }
-            int count = j-i;
-            //write the char first then we write the freq
+            int freq = j-i;
             chars[lastIndex++] = chars[i];
-            //write the freq
-            if(count > 1){
-                for(char c: String.valueOf(count).toCharArray()){
+            if(freq > 1){
+                //convert freq to string first and then to char array
+                for(char c: String.valueOf(freq).toCharArray()){
                     chars[lastIndex++] = c;
                 }
             }
-             i = j;
+            i = j;
         }
         return lastIndex;
-
-
     }
 }
