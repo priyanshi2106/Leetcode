0b536c030009f2be341fb5783cf22638fc38a5e7
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-03 23:55:00 -0400

    Time: 1 ms (97.7%), Space: 47.2 MB (45.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         //cant use arrays directly as we do not know the size of the ans array
-        ArrayList<int []> ans = new ArrayList<>();
+       ArrayList<int[]> ans = new ArrayList<>();
        int i = 0;
        //no overlapping
-       while(i < intervals.length && intervals[i][1] < newInterval[0]){
+       while(i<intervals.length && intervals[i][1] < newInterval[0]){
         ans.add(intervals[i]);
         i++;
        }
-
-       // if overlapping happens - calculate newInterval 
-       while( i < intervals.length && intervals[i][0] <= newInterval[1]){
-        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-        newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+       //overlapping
+       while( i < intervals.length && intervals[i][0]<= newInterval[1]){
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
         i++;
-
        }
        ans.add(newInterval);
-       //this makes sure even if we do not have overlapping case newInterval gets added
-
-       //no overlapping after we merge
-       while(i < intervals.length ){
+       while(i < intervals.length){
         ans.add(intervals[i]);
         i++;
        }
        return ans.toArray(new int[ans.size()][]);
-    }
+           }
 }
