79a4307cab78202340d59c856d02830c473025f9
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-04 15:28:40 -0400

    Time: 105 ms (24.94%), Space: 47 MB (16.85%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,48 +1,11 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        //t:O(n^2)
-        // int ans = 0;
-        // while(s.indexOf("01") >= 0){
-        //     s = s.replace("01", "10");
-        //     ans++;
-        // }
-        // return ans;
-
-    //     int ans = 0;
-    //     StringBuilder sb = new StringBuilder(s);
-    //     while(swapped(sb)){
-    //         ans ++;
-    //     }
-    //     return ans;
-    // }
-    // public boolean swapped(StringBuilder s){
-    //     boolean swappedFlag = false;
-    //     for(int i = 0; i < s.length() - 1; i++){
-    //         if(s.charAt(i) == '0' && s.charAt(i+1) == '1'){
-    //             s.setCharAt(i, '1');
-    //             s.setCharAt(i+1, '0');
-    //             i++;
-    //             swappedFlag = true;
-    //         }
-    //     }
-    //     return swappedFlag;
-
-    //T: O(n)
-
-    //we have to move every 1 to left from right and it takes atleast  number of 0s to left of one + 1 sec to do that.
-
-    //if two 1s are adjacent, the second 1 has to wait for the first 1 to move before it can move
-
-    int zeros = 0;
-    int sec = 0;
-    for(int i = 0; i < s.length() ; i++){
-        if(s.charAt(i) == '0'){
-            zeros++;
+        int ans = 0;
+        while(s.contains("01")){
+            ans++;
+            s = s.replace("01", "10");
         }
-        if(s.charAt(i) == '1' && zeros > 0){
-            sec = Math.max(sec + 1, zeros);
-        }
-    }
-    return sec;
+        return ans;
     }
 }
+
