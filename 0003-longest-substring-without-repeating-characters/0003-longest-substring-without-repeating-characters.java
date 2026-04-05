a5c90ba6e4a11e5285e2b79d1c6c1452b621a19c
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-05 16:36:00 -0400

    Time: 6 ms (69.43%), Space: 46.4 MB (42.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,23 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-       HashMap<Character, Integer> hm = new HashMap<>();
-       int i = 0; 
-       int j = 0;
-       int max = 0;
-       while(j < s.length()){
-        if(!hm.containsKey(s.charAt(j))){
-            hm.put(s.charAt(j), j);
-            j++;
-            max = Math.max(max, hm.size());
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        while (j < s.length()) {
+            if (!hm.containsKey(s.charAt(j))) {
+                hm.put(s.charAt(j), j);
+                max = Math.max(max, hm.size());
+                j++;
+            } else {
+                hm.remove(s.charAt(i));
+                i++;
+            }
 
         }
-        else{
-            hm.remove(s.charAt(i));
-            i++;
-        }
-       }
-       return max;
-        ////T: O(n)
+                return max;
+
+        //// T: O(n)
         //S: O(n) -> bcs worst case the substring is the whole original string itself
         // int a_pointer = 0;
         // int b_pointer = 0;
@@ -29,7 +29,7 @@ class Solution {
         //         hs.add(s.charAt(b_pointer));
         //         b_pointer++;
         //         max = Math.max(max, hs.size());
-                
+
         //     }
         //     else{
         //         hs.remove(s.charAt(a_pointer));
