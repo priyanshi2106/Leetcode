ad216cac496c9a4d27bf579c3377ece5aa7077f2
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-05 17:10:08 -0400

    Time: 14 ms (14.03%), Space: 46.2 MB (94.75%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -3,6 +3,10 @@ class Solution {
         //S: O(n)
         //T: O(nlogn)
 
+        //idea is to have a hashmap that stores the freq of each number
+        //create a new array with no duplicates and sort it so it is easier to cache the result
+        //go through the new array and see if we can take adjascent number or not
+
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int n : nums) {
             if (!hm.containsKey(n)) {
@@ -11,29 +15,29 @@ class Solution {
                 hm.put(n, hm.get(n) + 1);
             }
         }
-        //creat a points array that will not have duplicates
+
+        //instead of array define list so we can access the points in O(1)
         List<Integer> points = new ArrayList<>(hm.keySet());
         Collections.sort(points);
+        int[] dp = new int[points.size() + 1];
 
-        //loop through points to form a dp array
-        int dp[] = new int[points.size()];
+        dp[0] = points.get(0) * hm.get(points.get(0));
+        if (points.size() == 1) {
+            return dp[0];
+        }
+        dp[1] = (points.get(1) - 1 == points.get(0)) ? Math.max(dp[0], points.get(1) * hm.get(points.get(1)))
+                : dp[0] + points.get(1) * hm.get(points.get(1));
 
-        dp[0] = points.get(0)*hm.get(points.get(0));
-        if (points.size() == 1) return dp[0];  
-        
-        dp[1] = (points.get(1)-1 == points.get(0))? Math.max(dp[0], points.get(1)*hm.get(points.get(1)))  : points.get(1)*hm.get(points.get(1))+dp[0];
-
-        for(int i = 2; i < points.size(); i++){
-            if(points.get(i)-1 == points.get(i-1)){
+        for (int i = 2; i < points.size(); i++) {
+            if (points.get(i) - 1 == points.get(i - 1)) {
                 //adjascent numbers we cant take the right value
-                dp[i] = Math.max(dp[i-1], points.get(i)*hm.get(points.get(i)) + dp[i-2]); 
-            }
-            else{
-                //can take adjascent number
-                dp[i] = points.get(i)*hm.get(points.get(i)) + dp[i-1];
+                dp[i] = Math.max(dp[i - 1], points.get(i) * hm.get(points.get(i)) + dp[i - 2]);
+            } else {
+                 //can take adjascent number
+                dp[i] = dp[i - 1] + points.get(i) * hm.get(points.get(i));
             }
         }
         return dp[points.size()-1];
-       
+
     }
 }
