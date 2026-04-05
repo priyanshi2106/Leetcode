71126e8f15cc4d87e91cef82e50d0a112dafe0e0
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-05 15:06:26 -0400

    Time: 26 ms (27.94%), Space: 86 MB (89.17%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -2,18 +2,21 @@ class Solution {
     public int minRemoval(int[] nums, int k) {
         //condition to check: max <= k * min
         Arrays.sort(nums);
+        int i = 0;
+        int j = 0;
         int ans = 0;
-        int i = 0; int j = 0;
         while(j < nums.length){
             if((long)nums[i] * k >= nums[j]){
+                //now we can explore more max elements
                 j++;
                 ans = Math.max(ans, j-i);
+                //j-i gives us how many valid elements we have so far
             }
             else{
                 i++;
             }
         }
-        return nums.length -ans; 
+        return nums.length-ans;
         
     }
 }
