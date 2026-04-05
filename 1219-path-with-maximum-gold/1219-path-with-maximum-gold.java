d9a0f7f485e24d060eed6bdbba16507fdec1558b
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-05 18:24:07 -0400

    Time: 92 ms (25.17%), Space: 43.1 MB (50.75%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,33 +1,31 @@
 class Solution {
-      int directions[][] = {{0,1}, {0,-1}, {1,0}, {-1, 0}};
+    int directions[][] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
     public int getMaximumGold(int[][] grid) {
-      //dfs
-      int max = 0;
-      for(int i = 0; i < grid.length; i++){
-        for(int j = 0; j < grid[0].length; j++){
-            if(grid[i][j] != 0){
-                //we can explore further
-                max = Math.max(max, dfs(grid,i, j) );
+        int ans = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j< grid[0].length; j++){
+                if(grid[i][j] != 0){
+                    //explore this index and its neighbors
+                    ans = Math.max(ans,dfs(grid, i, j));
+                }
             }
         }
-      }
-      return max;
+        return ans;
     }
-    public int dfs(int grid[][], int i , int j){
+    public int dfs(int[][] grid, int i , int j){
         //base case
-        if( i< 0 || i >= grid.length || j >=grid[0].length ||j < 0 || grid[i][j] == 0){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
             return 0;
         }
         int temp = grid[i][j];
-        //mark it as visited for now
+        int maxAns = 0;
+        //mark this as visited for now
         grid[i][j] = 0;
-        int maxGold = 0;
-        //explore other directions through this element
+        //explore the other directions
         for(int dir[] : directions){
-            maxGold = Math.max(maxGold,dfs(grid, i + dir[0], j + dir[1]));
+            maxAns = Math.max(maxAns, dfs(grid, i + dir[0], j + dir[1]));
         }
-        //restore cell as we have explored directions
         grid[i][j] = temp;
-        return temp+maxGold;
+        return maxAns+temp;
     }
 }
