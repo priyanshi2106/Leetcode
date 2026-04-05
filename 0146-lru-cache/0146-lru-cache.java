9938bd3581c6e1165e1b0e15ef7aed80fc25583e
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-04-05 15:48:52 -0400

    Time: 46 ms (62.69%), Space: 133.9 MB (12.44%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -2,68 +2,65 @@ class LRUCache {
     int capacity;
     ListNode head;
     ListNode tail;
-    HashMap<Integer,ListNode> hm ;
+    HashMap<Integer, ListNode> hm;
+
     public LRUCache(int capacity) {
-        this.capacity = capacity;
         hm = new HashMap<>();
-        head = new ListNode(-1,-1);
-        tail = new ListNode(-1,-1);
+        this.capacity = capacity;
+        head = new ListNode(-1, -1);
+        tail = new ListNode(-1, -1);
         head.next = tail;
         tail.prev = head;
-        
     }
-    
+
     public int get(int key) {
-        if(hm.containsKey(key)){
+        if (hm.containsKey(key)) {
             ListNode node = hm.get(key);
             remove(node);
             add(node);
             return node.value;
-
-        }
-        else{
+        } else {
             return -1;
         }
-        
     }
-    
+
     public void put(int key, int value) {
-        if(hm.containsKey(key)){
+        if (hm.containsKey(key)) {
             ListNode oldNode = hm.get(key);
             remove(oldNode);
         }
         ListNode newNode = new ListNode(key, value);
-        add(newNode);
-        //update hm
         hm.put(key, newNode);
-        if(hm.size() > capacity){
-            //delete node
+        add(newNode);
+        if (hm.size() > capacity) {
+            //delete node at head
             ListNode nodeToDelete = head.next;
             remove(nodeToDelete);
             hm.remove(nodeToDelete.key);
         }
-        
     }
-    public void add(ListNode node){
+
+    public void add(ListNode node) {
         ListNode prevEnd = tail.prev;
         prevEnd.next = node;
+        tail.prev = node;
         node.prev = prevEnd;
         node.next = tail;
-        tail.prev = node;
     }
     public void remove(ListNode node){
         node.prev.next = node.next;
         node.next.prev = node.prev;
-
     }
-    public class ListNode{
+
+    public class ListNode {
         int key;
         int value;
         ListNode prev;
         ListNode next;
-        public ListNode(int key, int value){
-           this.key = key;
-           this.value = value;
+
+        ListNode(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
 }
