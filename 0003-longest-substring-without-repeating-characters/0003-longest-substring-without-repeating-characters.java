e7bbe7ca358ac4a9026ee84762debdc63232fad0
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-06-18 21:40:00 -0400

    Time: 6 ms (69.49%), Space: 46.8 MB (13.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,42 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        int max = 0;
-        while (j < s.length()) {
-            if (!hm.containsKey(s.charAt(j))) {
-                hm.put(s.charAt(j), j);
-                max = Math.max(max, hm.size());
-                j++;
-            } else {
-                hm.remove(s.charAt(i));
-                i++;
-            }
-
+       if(s.length() == 1){
+        return 1;
+       }
+       HashMap<Character, Integer> hm = new HashMap<>();
+       int i = 0;
+       int j = 0;
+       int ans = 0;
+       while(j < s.length()){
+        if(!hm.containsKey(s.charAt(j))){
+            hm.put(s.charAt(j), j);
+            ans = Math.max(ans, hm.size());
+            j++;
         }
-                return max;
-
-        //// T: O(n)
-        //S: O(n) -> bcs worst case the substring is the whole original string itself
-        // int a_pointer = 0;
-        // int b_pointer = 0;
-        // int max = 0;
-        // HashSet<Character> hs = new HashSet<>();
-        // //using hashset because lookup time for hashset is O(1)
-        // while(b_pointer < s.length()){
-        //     if(!hs.contains(s.charAt(b_pointer))){
-        //         hs.add(s.charAt(b_pointer));
-        //         b_pointer++;
-        //         max = Math.max(max, hs.size());
-
-        //     }
-        //     else{
-        //         hs.remove(s.charAt(a_pointer));
-        //         //max--;
-        //         a_pointer++;
-        //     }
-        // }
-        // return max;
+        else{
+            hm.remove(s.charAt(i));
+            i++;
+        }
+       }
+       return ans;
     }
 }
