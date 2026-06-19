04860b62384187739b6667c19507a78b89e5fb2b
Priyanshi <62298120+priyanshi2106@users.noreply.github.com>
2026-06-19 16:47:52 -0400

    Time: 34 ms (60.59%), Space: 59.2 MB (45.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,29 +4,31 @@ class Solution {
         //time compl: O(n^2) in best case
         //Space: O(n)
         Arrays.sort(nums);
+
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length-2;i++){
+        for(int i = 0; i < nums.length-2; i++){
             if(i == 0 || i > 0 && nums[i] != nums[i-1]){
                 int left = i+1;
                 int right = nums.length-1;
-                int sum = 0-nums[i];
+                int target = 0-nums[i];
                 while(left < right){
-                    if( nums[left] + nums[right] == sum){
+                    if(nums[left] + nums[right] == target){
                         ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while(left < right && nums[left] == nums[left+1]){
+                        //check for duplicates
+                        while(left < right && nums[left] == nums[left+1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right-1]){
+                            right--;
+                        }
                         left++;
-                    }
-                    while(left < right && nums[right] == nums[right-1]){
                         right--;
                     }
-                    left++;
-                    right--;
-                    }
-                    else if(nums[left] + nums[right] > sum){
-                        right--;
+                    else if(nums[left] + nums[right] < target){
+                        left++;
                     }
                     else{
-                        left++;
+                        right--;
                     }
                 }
             }
